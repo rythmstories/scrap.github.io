f0740775103aea089d38ff479dd8b8b475304e71
Gaurav Gupta <68333052+rythmstories@users.noreply.github.com>
2020-11-08 13:36:27 +0530

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import webs, check, suggestionyellow
+from .views import webs, suggestionyellow
 
 urlpatterns = [
     url(r'websyellow/',webs, name="create"),
-    url(r'check',check, name="create"),
     url(r'suggestion',suggestionyellow, name="create"),
 
 ]
